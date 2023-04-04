b48f2fc5bd4e9ed513aea236c100d9ea3c4f1ecb
thelastandrew <thelastandrew@gmail.com>
2023-04-04 20:53:35 +0300

    feat: add 404 page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,88 @@
 import { FC } from 'react';
 
+import {
+  createStyles,
+  Image,
+  Container,
+  Title,
+  Text,
+  Button,
+  SimpleGrid,
+  rem,
+} from '@mantine/core';
+import { Link } from 'react-router-dom';
+
+import image from 'assets/404.svg';
+
+const useStyles = createStyles((theme) => ({
+  root: {
+    paddingTop: rem(80),
+    paddingBottom: rem(80),
+  },
+
+  title: {
+    fontWeight: 900,
+    fontSize: rem(34),
+    marginBottom: theme.spacing.md,
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+
+    [theme.fn.smallerThan('sm')]: {
+      fontSize: rem(32),
+    },
+  },
+
+  control: {
+    [theme.fn.smallerThan('sm')]: {
+      width: '100%',
+    },
+  },
+
+  mobileImage: {
+    [theme.fn.largerThan('sm')]: {
+      display: 'none',
+    },
+  },
+
+  desktopImage: {
+    [theme.fn.smallerThan('sm')]: {
+      display: 'none',
+    },
+  },
+}));
+
 const NotFound: FC = () => {
-  return <div>404</div>;
+  const { classes } = useStyles();
+
+  return (
+    <Container className={classes.root}>
+      <SimpleGrid
+        spacing={80}
+        cols={2}
+        breakpoints={[{ maxWidth: 'sm', cols: 1, spacing: 40 }]}
+      >
+        <Image src={image} className={classes.mobileImage} />
+        <div>
+          <Title className={classes.title}>Something is not right...</Title>
+          <Text color="dimmed" size="lg">
+            Page you are trying to open does not exist. You may have mistyped the address,
+            or the page has been moved to another URL. If you think this is an error
+            contact support.
+          </Text>
+          <Button
+            variant="outline"
+            size="md"
+            mt="xl"
+            className={classes.control}
+            component={Link}
+            to="/"
+          >
+            Get back to home page
+          </Button>
+        </div>
+        <Image src={image} className={classes.desktopImage} />
+      </SimpleGrid>
+    </Container>
+  );
 };
 
 export default NotFound;
