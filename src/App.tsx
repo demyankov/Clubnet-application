6a67819a70eeee6dfba95800c04422d6ff8efb40
thelastandrew <thelastandrew@gmail.com>
2023-04-04 08:22:44 +0300

    feat: add header with nav links

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 
 import { Routes, Route } from 'react-router-dom';
 
+import { HeaderMegaMenu } from 'components/Header';
 import Dashboard from 'pages/Dashboard';
 import Home from 'pages/Home';
 import Login from 'pages/Login';
@@ -11,14 +12,17 @@ import Register from 'pages/Register';
 
 const App: FC = () => {
   return (
-    <Routes>
-      <Route index element={<Home />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <>
+      <HeaderMegaMenu />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
 };
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,152 @@
+import { FC } from 'react';
+
+import {
+  createStyles,
+  Header,
+  Group,
+  Button,
+  Divider,
+  Box,
+  Burger,
+  Drawer,
+  ScrollArea,
+  rem,
+} from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { DiReact } from 'react-icons/di';
+import { Link } from 'react-router-dom';
+
+const useStyles = createStyles((theme) => ({
+  link: {
+    display: 'flex',
+    alignItems: 'center',
+    height: '100%',
+    paddingLeft: theme.spacing.md,
+    paddingRight: theme.spacing.md,
+    textDecoration: 'none',
+    color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+    fontWeight: 500,
+    fontSize: theme.fontSizes.sm,
+
+    [theme.fn.smallerThan('sm')]: {
+      height: rem(42),
+      display: 'flex',
+      alignItems: 'center',
+      width: '100%',
+    },
+
+    ...theme.fn.hover({
+      backgroundColor:
+        theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+    }),
+  },
+
+  subLink: {
+    width: '100%',
+    padding: `${theme.spacing.xs} ${theme.spacing.md}`,
+    borderRadius: theme.radius.md,
+
+    ...theme.fn.hover({
+      backgroundColor:
+        theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.colors.gray[0],
+    }),
+
+    '&:active': theme.activeStyles,
+  },
+
+  dropdownFooter: {
+    backgroundColor:
+      theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.colors.gray[0],
+    margin: `calc(${theme.spacing.md} * -1)`,
+    marginTop: theme.spacing.sm,
+    padding: `${theme.spacing.md} calc(${theme.spacing.md} * 2)`,
+    paddingBottom: theme.spacing.xl,
+    borderTop: `${rem(1)} solid ${
+      theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[1]
+    }`,
+  },
+
+  hiddenMobile: {
+    [theme.fn.smallerThan('sm')]: {
+      display: 'none',
+    },
+  },
+
+  hiddenDesktop: {
+    [theme.fn.largerThan('sm')]: {
+      display: 'none',
+    },
+  },
+}));
+
+export const HeaderMegaMenu: FC = () => {
+  const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] =
+    useDisclosure(false);
+  const { classes, theme } = useStyles();
+
+  return (
+    <Box pb={120}>
+      <Header height={60} px="md">
+        <Group position="apart" sx={{ height: '100%' }}>
+          <DiReact size={40} />
+
+          <Group sx={{ height: '100%' }} spacing={0} className={classes.hiddenMobile}>
+            <Link to="/" className={classes.link}>
+              Home
+            </Link>
+            <Link to="/dashboard" className={classes.link}>
+              Dashboard
+            </Link>
+          </Group>
+
+          <Group className={classes.hiddenMobile}>
+            <Button component={Link} to="/login" variant="default">
+              Log in
+            </Button>
+            <Button component={Link} to="/register">
+              Sign up
+            </Button>
+          </Group>
+
+          <Burger
+            opened={drawerOpened}
+            onClick={toggleDrawer}
+            className={classes.hiddenDesktop}
+          />
+        </Group>
+      </Header>
+
+      <Drawer
+        opened={drawerOpened}
+        onClose={closeDrawer}
+        size="100%"
+        padding="md"
+        title="Navigation"
+        className={classes.hiddenDesktop}
+        zIndex={1000000}
+      >
+        <ScrollArea h={`calc(100vh - ${rem(60)})`} mx="-md">
+          <Divider my="sm" color={theme.colorScheme === 'dark' ? 'dark.5' : 'gray.1'} />
+
+          <Link onClick={closeDrawer} to="/" className={classes.link}>
+            Home
+          </Link>
+          <Link onClick={closeDrawer} to="/dashboard" className={classes.link}>
+            Dashboard
+          </Link>
+
+          <Divider my="sm" color={theme.colorScheme === 'dark' ? 'dark.5' : 'gray.1'} />
+
+          <Group position="center" grow pb="xl" px="md">
+            <Button onClick={closeDrawer} component={Link} to="/login" variant="default">
+              Log in
+            </Button>
+            <Button onClick={closeDrawer} component={Link} to="/register">
+              Sign up
+            </Button>
+          </Group>
+        </ScrollArea>
+      </Drawer>
+    </Box>
+  );
+};
